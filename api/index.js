216618e7f217defb8dbce4f38956daa31d307d31
Divyansh Bansal <bansal9divyansh@gmail.com>
2024-10-28 21:46:12 +0530

    error handling

--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = process.env.JWT_SECRET || 'defaultSecret';
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
