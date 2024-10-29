7292186ed9e47ba70a7ad57e2944fd604d0f04b2
Divyansh Bansal <bansal9divyansh@gmail.com>
2024-10-29 15:42:37 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -21,14 +21,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-<<<<<<< HEAD
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-=======
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000', 'https://mernblog-ecru.vercel.app']
+  origin: ['http://localhost:3000']
 }));
->>>>>>> 53ff4b9a323b507a93aafc1e34c7405a2defd046
 app.use(express.json());
 app.use(cookieParser());
 
